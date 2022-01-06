51cdf5f4aae18f49257b48d296bd3d15aed969a8
Ronald Thian <roarnald@gmail.com>
2022-01-06 23:31:06 +0800

    feat: Skeleton Setup for react and ts

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import React, { FC } from 'react';
+import { render } from 'react-dom';
+
+const Application: FC = () => <h1>Application</h1>;
+
+render(<Application />, document.getElementById('root'));

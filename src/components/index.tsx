0316c9c1344ef3d65036cb48006d13e6da67e12a
roarnald <roarnald@gmail.com>
2022-01-12 13:35:18 +0800

    fix: Routes for redirect to /bread-tracker path

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,19 +2,24 @@ import 'css/main.css';
 
 import React, { FC } from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 import lazy from 'utils/lazy';
 
 const Home = lazy(() => import('./Home'));
 
 const Application: FC = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/bread-tracker" element={<Home />} />
-    </Routes>
-  </BrowserRouter>
+  <>
+    <BrowserRouter basename="/">
+      <Routes>
+        <Route path="/bread-tracker">
+          <Route index element={<Home />} />
+        </Route>
+
+        <Route path="*" element={<Navigate replace to="/bread-tracker" />} />
+      </Routes>
+    </BrowserRouter>
+  </>
 );
 
 render(<Application />, document.getElementById('root'));

4ec8ea9f7d1615386acf34ff9f9d8dd770625bfa
roarnald <roarnald@gmail.com>
2022-01-18 17:57:11 +0800

    feat: Remove /bread-tracker redirect for custom domain

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,7 +3,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 import React, { FC } from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
 import lazy from '~/src/utils/lazy';
@@ -14,11 +14,9 @@ const Application: FC = () => (
   <>
     <BrowserRouter basename="/">
       <Routes>
-        <Route path="/bread-tracker">
+        <Route path="/">
           <Route index element={<Home />} />
         </Route>
-
-        <Route path="*" element={<Navigate replace to="/bread-tracker" />} />
       </Routes>
     </BrowserRouter>
 
